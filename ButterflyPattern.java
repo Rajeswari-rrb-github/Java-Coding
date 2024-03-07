60e57742f460ea5328b503f7cd8f85ad78c4ca64
Rajeswari-rrb-github <109688997+Rajeswari-rrb-github@users.noreply.github.com>
2024-03-07 14:48:26 +0530

    Butterfly Pattern in java

---
+++ ButterflyPattern.java
@@ -0,0 +1,44 @@
+public class ButterflyPattern {
+    public static void butterfly(int n){
+        // 1st half
+        // -----outer loop-----
+        for(int i=1; i<=n; i++){
+            // stars =i
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+            // spaces= 2(n-i)
+            for(int j=1; j<=2*(n-i); j++){
+                System.out.print(" ");
+            }
+            // stars= i
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        // 2nd half
+        // outer loop
+        for(int i= n; i>=1; i--){
+            // stars =i
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+            // spaces= 2(n-i)
+            for(int j=1; j<=2*(n-i); j++){
+                System.out.print(" ");
+            }
+            // stars= i
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        //System.out.println();
+        butterfly(10);
+    }
+}
