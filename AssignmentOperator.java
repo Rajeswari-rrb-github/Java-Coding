d364519bf88009893249b434d4e242ece77cd211
Rajeswari-rrb-github <109688997+Rajeswari-rrb-github@users.noreply.github.com>
2024-03-07 14:41:49 +0530

    Assignment Operators in java

---
+++ AssignmentOperator.java
@@ -0,0 +1,11 @@
+public class AssignmentOperator {
+    public static void main(String[] args) {
+        int a = 9;
+        System.out.println(a += 9);
+        System.out.println(a -= 9);
+        System.out.println(a *= 9);
+        System.out.println(a /= 9);
+    }
+    
+    
+}
