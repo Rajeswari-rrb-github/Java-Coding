10d9e1541121a96b422d0097985dac16a347d7ea
Rajeswari-rrb-github <109688997+Rajeswari-rrb-github@users.noreply.github.com>
2024-03-07 14:38:52 +0530

    ArithmaticOperators_Binary.java

---
+++ ArithmaticOperators_Binary.java
@@ -0,0 +1,11 @@
+public class ArithmaticOperators_Binary {
+    public static void main(String[] args) {
+        int a= 10;
+        int b= 5;
+        System.out.println("addition: " +(a+b));
+        System.out.println("subtraction: " +(a-b));
+        System.out.println("multiplication: " +(a*b));
+        System.out.println("division: " +(a/b));
+        System.out.println("modulo division: " +(a%b));
+    }
+}
