0e697aba70294da555259db88df1772fcdb4267a
Rajeswari-rrb-github <109688997+Rajeswari-rrb-github@users.noreply.github.com>
2024-03-07 14:38:03 +0530

    Create AddThree_numbers.java

---
+++ AddThree_numbers.java
@@ -0,0 +1,9 @@
+public class AddThree_numbers {
+    public static void main(String[] args) {
+        int a = 5;
+        double b= 0.4;
+        double c=8.9;
+        double sum = a+b+c;
+        System.out.println("The sum of three numbers is: " + sum);
+    }
+}
