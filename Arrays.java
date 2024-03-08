def264fc69cd1defb51c2ef31557905bd0143ca2
Rajrajeswari <109688997+Rajeswari-rrb-github@users.noreply.github.com>
2024-03-09 01:11:17 +0530

    Arrays in Java

---
+++ Arrays.java
@@ -0,0 +1,11 @@
+public class Arrays {
+    public static void main(String[] args) {
+        // class of 500 stdents- store their numbers- using arrays
+        // Array- collection of similar type of data type
+        int[] marks= new int[5];
+        marks[0]=90;
+        marks[1]=98;
+        
+
+    }
+}
