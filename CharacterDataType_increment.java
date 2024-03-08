e4c42ba7771815f351cf221f652794f2fe3649a8
Rajrajeswari <109688997+Rajeswari-rrb-github@users.noreply.github.com>
2024-03-09 01:13:45 +0530

    Character Data Type increment

---
+++ CharacterDataType_increment.java
@@ -0,0 +1,13 @@
+public class CharacterDataType_increment {
+    public static void main(String[] args) {
+        char ch1, ch2;
+        ch1= 88;
+        ch2= 'Y';
+        System.out.print("ch1 and ch2 are : ");
+        System.out.println(ch1 + " " + ch2);
+        ch1++;
+        ch2++;
+        System.out.print("Now ch1 and ch2 after increment are : ");
+        System.out.print(ch1 + " " + ch2);
+    }
+}
