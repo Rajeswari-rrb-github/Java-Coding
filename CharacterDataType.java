212150bebc1b5ed361b29c4aebda3dd39358094e
Rajrajeswari <109688997+Rajeswari-rrb-github@users.noreply.github.com>
2024-03-09 01:12:49 +0530

    Character data type

---
+++ CharacterDataType.java
@@ -0,0 +1,10 @@
+public class CharacterDataType {
+    public static void main(String[] args) {
+        char ch1, ch2;
+        ch1= 88;
+        ch2= 'Y';
+        System.out.print("ch1 and ch2 are : ");
+        System.out.print(ch1 + " " + ch2);
+    }
+    
+}
