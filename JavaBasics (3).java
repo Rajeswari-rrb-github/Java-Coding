1a137060c8c60ff50dce8dbc7decbfed03f8c98a
Rajrajeswari <109688997+Rajeswari-rrb-github@users.noreply.github.com>
2024-03-09 01:43:40 +0530

    Java

---
+++ JavaBasics (3).java
@@ -0,0 +1,7 @@
+public class JavaBasics {
+    public static void main(String args[]) {
+      System.out.print("Hello World!"); 
+
+    }
+}
+//  boilerplate code- to be written always in every code
